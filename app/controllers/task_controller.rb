83bbfd5c0ff14954b2337a89f59fdf64a34d418c
Maxim Kaschenko <KaschenkoMaksim@gmail.com>
2009-10-06 22:30:28 +0700

    Done Tasks List

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,13 +1,33 @@
 class TaskController < ApplicationController
 
   def index
-    @tasks = Task.find(:all, :group => "title")
+    @tasks = Task.find_uncomplete_tasks
+    @complete_tasks = Task.find_complete_tasks
   end
 
   def create
     @task = Task.new(params[:task])
-    @task.save
-    render :partial => "list", :locals => { :tasks => Task.find(:all, :group => "title") }
+    if @task.save
+      render :update do |page|
+        page.replace_html 'tasks_list', :partial => "list", :locals => { :tasks => Task.find_uncomplete_tasks }
+        page.select('div.inputError').hide()
+        page.visual_effect(:highlight, "task_#{@task.id}", :duration => 1)
+      end
+    else
+      render :update do |page|
+        page.replace_html 'task_error', (error_message_on(:task, :title, :prepend_text => "Title ", :css_class => "inputError"))
+      end
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attribute(:complete, params[:complete])
+    render :update do |page|
+      page.replace_html 'tasks_list', :partial => "list", :locals => { :tasks => Task.find_uncomplete_tasks }
+      page.replace_html 'list_complete', :partial => "list_complete", :locals => { :tasks => Task.find_complete_tasks }
+      page.visual_effect(:highlight, "task_#{@task.id}", :duration => 1)
+    end
   end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,13 @@ class Task < ActiveRecord::Base
 
   validates_presence_of :title
   validates_uniqueness_of :title
+
+  def self.find_uncomplete_tasks
+    find(:all, :conditions => ['complete = ?', false], :group => 'title')
+  end
+
+  def self.find_complete_tasks
+    find(:all, :conditions => ['complete = ?', true], :group => 'title')
+  end
+
 end

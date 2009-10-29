e2a66986561f9174b7666b5f2f0eaf6833d2c66a
Maxim Kaschenko <KaschenkoMaksim@gmail.com>
2009-10-30 02:33:40 +0600

    Deleted Unnecessary Files

--- db/migrate/20091010143713_change_spent_time_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeSpentTimeType < ActiveRecord::Migration
-  def self.up
-    change_column :tasks, :spent_time, :integer, :default => 0
-  end
-
-  def self.down
-    change_column :tasks, :spent_time, :time, :default => 0
-  end
-end
